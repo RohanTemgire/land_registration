be604e5db9713474ad4a5d3a25c080de8d208262
Rohan Temgire <temgirerrohan@gmail.com>
2023-04-10 21:12:29 +0530

    inital_commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,11 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEYwRGY1NUQ0NzMwOUMwMUQ5NDQ4NDc3NzhBRDEzN2E3RjAyMkIzMTEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3OTQ2NzYzMzg3MSwibmFtZSI6ImxhbmQgcmVnaXN0ZXIifQ.S7GCcT7TNpywtdiWU09YGlffItshT4ULjONZAijfFQI';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+
+const String contractAddress = "0xfE2fcDaB945fd4D1924E2FdDf6efFf5F86Ab88a2";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
